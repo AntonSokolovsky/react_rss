00499216cfa684876eea04c7a8e063da660cb299
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2023-11-04 14:13:23 +0300

    refactor: change main directory

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Component, ReactNode } from 'react';
+import styles from './App.module.css';
+import SearchBar from './components/SearchBar/Search';
+import Cards from './components/Cards/Cards';
+
+export default class App extends Component {
+  render(): ReactNode {
+    return (
+      <div className={styles.myApp}>
+        <h1>StarTrek</h1>
+        <SearchBar />
+        <Cards />
+      </div>
+    );
+  }
+}
